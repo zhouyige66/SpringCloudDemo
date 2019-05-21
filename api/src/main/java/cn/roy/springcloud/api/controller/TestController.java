933086ecafcd597af5b6a6e3e2075a5bc48a82bb
Roy Z Zhou <roy.z.zhou@cn.pwc.com>
2019-05-21 17:33:48 +0800

    修改配置文件

--- api/src/main/java/cn/roy/springcloud/api/controller/TestController.java
+++ api/src/main/java/cn/roy/springcloud/api/controller/TestController.java
@@ -1,5 +1,6 @@
 package cn.roy.springcloud.api.controller;
 
+import cn.roy.springcloud.api.entity.User;
 import cn.roy.springcloud.api.util.MailUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -7,9 +8,7 @@ import org.apache.commons.collections4.map.HashedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,21 +47,21 @@ public class TestController {
      * notes:接口描述
      * tags:用于接口分类，相当于按tag分组
      */
-    @ApiOperation(value = "我是hello接口", notes = "这是hello接口描述", tags = {"tag1"})
-    @GetMapping("hello")
-    public String hello() {
+    @ApiOperation(value = "接口名称", notes = "功能：接口功能描述", tags = {"tag1"})
+    @PostMapping("hello")
+    public String hello(@RequestBody User user) {
         Map<String, Object> map = new HashedMap<>();
         map.put("name", "kk20");
-        return mailUtil.processThymeleafTemplateIntoString("tt.html", map);
+        return mailUtil.processThymeleafTemplateIntoString("entity.html", map);
     }
 
-    @ApiOperation(value = "我是name接口value", notes = "我是name接口notes", tags = {"tag1"})
+    @ApiOperation(value = "接口名称", notes = "功能：接口功能描述", tags = {"tag1"})
     @GetMapping("hello2")
     public String getUserName() {
         return "I am api，端口：" + port + "，从配置中心读取的名字是：" + userName;
     }
 
-    @ApiOperation(value = "发送文本邮件", notes = "发送文本邮件", tags = {"邮件"})
+    @ApiOperation(value = "发送文本邮件", notes = "功能：发送文本邮件", tags = {"邮件"})
     @GetMapping("send")
     public String sendSimpleMail() {
         String content = "这是一封简单的测试邮件";
@@ -75,7 +74,7 @@ public class TestController {
         }
     }
 
-    @ApiOperation(value = "发送附件邮件", notes = "发送附件邮件", tags = {"邮件"})
+    @ApiOperation(value = "发送附件邮件", notes = "功能：发送附件邮件", tags = {"邮件"})
     @GetMapping("send2")
     public String sendAttachmentsMail() {
         String filePath = "C:\\Users\\Roy Z Zhou\\Desktop\\log.xls";
@@ -92,7 +91,7 @@ public class TestController {
         }
     }
 
-    @ApiOperation(value = "发送模板邮件（带附件）", notes = "发送模板邮件（带附件）", tags = {"邮件"})
+    @ApiOperation(value = "发送模板邮件（带附件）", notes = "功能：发送模板邮件（带附件）", tags = {"邮件"})
     @GetMapping("send3")
     public String sendEmail() {
         String filePath = "C:\\Users\\Roy Z Zhou\\Desktop\\log.xls";
---
+++ api/src/main/java/cn/roy/springcloud/api/entity/User.java
@@ -0,0 +1,45 @@
+package cn.roy.springcloud.api.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * @Description: 用户
+ * @Author: Roy Z
+ * @Date: 2019-05-17 13:59
+ * @Version: v1.0
+ */
+@ApiModel
+public class User {
+
+    @ApiModelProperty(value = "名字")
+    private String name;
+    @ApiModelProperty(value = "年龄")
+    private int age;
+    @ApiModelProperty(value = "性别")
+    private int sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+}
