f27f74ee9eb0e618933c4107ef279bd69a5fdb3b
Roy Z Zhou <roy.z.zhou@cn.pwc.com>
2019-09-17 09:24:49 +0800

    ESRest配置更改

--- gateway/src/main/java/cn/roy/springcloud/gateway/config/ESRestConfig.java
+++ gateway/src/main/java/cn/roy/springcloud/gateway/config/ESRestConfig.java
@@ -37,8 +37,6 @@ public class ESRestConfig {
         RestClientBuilder clientBuilder = RestClient.builder(new HttpHost("localhost", 9200,
                 "http"));
 
-        // 添加其他配置，返回来的还是RestClientBuilder对象，这些配置都是可选的
-//        clientBuilder.setXX();
         // 设置请求头，每个请求都会带上这个请求头
         Header[] defaultHeaders = {new BasicHeader("header", "value")};
         clientBuilder.setDefaultHeaders(defaultHeaders);
@@ -52,9 +50,9 @@ public class ESRestConfig {
 //                System.out.println(node.getName() + "==节点失败了");
 //            }
 //        });
-        /** 配置节点选择器，客户端以循环方式将每个请求发送到每一个配置的节点上，
-         * 发送请求的节点，用于过滤客户端，将请求发送到这些客户端节点，默认向每个配置节点发送，
-         * 这个配置通常是用户在启用嗅探时向专用主节点发送请求（即只有专用的主节点应该被HTTP请求命中）
+        /**
+         * 配置节点选择器，客户端以循环方式将每个请求发送到每一个配置的节点上，发送请求的节点，用于过滤客户端，将请求发送到这些客户端节点，
+         * 默认向每个配置节点发送，这个配置通常是用户在启用嗅探时向专用主节点发送请求（即只有专用的主节点应该被HTTP请求命中）
          */
         clientBuilder.setNodeSelector(NodeSelector.SKIP_DEDICATED_MASTERS);
         // 进行详细的配置
@@ -82,10 +80,11 @@ public class ESRestConfig {
                 }
             }
         });
-        /* 配置异步请求的线程数量，Apache Http Async Client默认启动一个调度程序线程，以及由连接管理器使用的许多工作线程
-        （与本地检测到的处理器数量一样多，取决于Runtime.getRuntime().availableProcessors()返回的数量）。线程数可以修改如下,
-        这里是修改为1个线程，即默认情况
-        */
+        /**
+         * 配置异步请求的线程数量，Apache Http Async Client默认启动一个调度程序线程，以及由连接管理器使用的许多工作线程（与本地检测到的
+         * 处理器数量一样多，取决于Runtime.getRuntime().availableProcessors()返回的数量）。线程数可以修改如下,这里是修改为1个线程，
+         * 即默认情况
+         */
         clientBuilder.setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
             @Override
             public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
@@ -94,10 +93,10 @@ public class ESRestConfig {
                 );
             }
         });
-        /*
-        配置请求超时，将连接超时（默认为1秒）和套接字超时（默认为30秒）增加，
-        这里配置完应该相应地调整最大重试超时（默认为30秒），即上面的setMaxRetryTimeoutMillis，一般于最大的那个值一致即60000
-        */
+        /**
+         * 配置请求超时，将连接超时（默认为1秒）和套接字超时（默认为30秒）增加，这里配置完应该相应地调整最大重试超时（默认为30秒），即上面的
+         * setMaxRetryTimeoutMillis，一般于最大的那个值一致即60000
+         */
         clientBuilder.setRequestConfigCallback(new RestClientBuilder.RequestConfigCallback() {
             @Override
             public RequestConfig.Builder customizeRequestConfig(RequestConfig.Builder requestConfigBuilder) {
@@ -105,9 +104,9 @@ public class ESRestConfig {
                 return requestConfigBuilder.setConnectTimeout(5000).setSocketTimeout(60000);
             }
         });
-        /*
-        如果ES设置了密码，那这里也提供了一个基本的认证机制，下面设置了ES需要基本身份验证的默认凭据提供程序
-            */
+        /**
+         * 如果ES设置了密码，那这里也提供了一个基本的认证机制，下面设置了ES需要基本身份验证的默认凭据提供程序
+         */
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY,
                 new UsernamePasswordCredentials("user", "password"));
@@ -117,11 +116,10 @@ public class ESRestConfig {
                 return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
             }
         });
-
-        /*
-        上面采用异步机制实现抢先认证，这个功能也可以禁用，这意味着每个请求都将在没有授权标头的情况下发送，然后查看它是否被接受，
-        并且在收到HTTP 401响应后，它再使用基本认证头重新发送完全相同的请求，这个可能是基于安全、性能的考虑
-            */
+        /**
+         * 上面采用异步机制实现抢先认证，这个功能也可以禁用，这意味着每个请求都将在没有授权标头的情况下发送，然后查看它是否被接受，并且在收到
+         * HTTP 401响应后，它再使用基本认证头重新发送完全相同的请求，这个可能是基于安全、性能的考虑
+         */
         clientBuilder.setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
             @Override
             public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
@@ -130,7 +128,9 @@ public class ESRestConfig {
                 return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
             }
         });
-        // 配置通信加密，有多种方式：setSSLContext、setSSLSessionStrategy和setConnectionManager(它们的重要性逐渐递增)
+        /**
+         * 配置通信加密，有多种方式：setSSLContext、setSSLSessionStrategy和setConnectionManager(它们的重要性逐渐递增)
+         */
 //        KeyStore truststore = KeyStore.getInstance("jks");
 //        try (InputStream is = Files.newInputStream(keyStorePath)) {
 //            truststore.load(is, keyStorePass.toCharArray());
@@ -147,4 +147,5 @@ public class ESRestConfig {
         // 最后配置好的clientBuilder再build一下即可得到真正的Client
         return clientBuilder.build();
     }
+
 }
