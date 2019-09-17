8d0476f5355283c620270694ddf40e46b7957065
Roy Z Zhou <roy.z.zhou@cn.pwc.com>
2019-09-17 14:48:14 +0800

    去除spring data elasticsearch

--- gateway/src/main/java/cn/roy/springcloud/gateway/es/bean/ApiCallBean.java
+++ gateway/src/main/java/cn/roy/springcloud/gateway/es/bean/ApiCallBean.java
@@ -1,10 +1,6 @@
 package cn.roy.springcloud.gateway.es.bean;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
-
 import java.util.Date;
-import java.util.UUID;
 
 /**
  * @Description: Api调用
@@ -12,24 +8,12 @@ import java.util.UUID;
  * @Date: 2019-09-13 15:21
  * @Version: v1.0
  */
-@Document(indexName = "ApiCall", type = "doc")
-public class ApiCallBean {
-    @Id
-    private String id = UUID.randomUUID().toString();// id必须
-
+public class ApiCallBean extends ESBaseBean{
     private String name;
     private Date startTime;
     private Date endTime;
     private Long costTime;
     private String remoteIp;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
---
+++ gateway/src/main/java/cn/roy/springcloud/gateway/es/bean/ESBaseBean.java
@@ -0,0 +1,49 @@
+package cn.roy.springcloud.gateway.es.bean;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * @Description: ES存储实体基类
+ * @Author: Roy Z
+ * @Date: 2019-09-17 13:26
+ * @Version: v1.0
+ */
+public class ESBaseBean {
+    private String id = UUID.randomUUID().toString();
+    private Date createDate;
+    private Date updateDate;
+    private Boolean del;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public Boolean getDel() {
+        return del;
+    }
+
+    public void setDel(Boolean del) {
+        this.del = del;
+    }
+}
