5210d5e2d76bdf919b1cd7dca396979fcffbaf08
Roy <751664206@qq.com>
2019-09-17 17:10:07 +0800

    es存储测试通过

--- gateway/src/main/java/cn/roy/springcloud/gateway/filter/PostFilter.java
+++ gateway/src/main/java/cn/roy/springcloud/gateway/filter/PostFilter.java
@@ -1,10 +1,16 @@
 package cn.roy.springcloud.gateway.filter;
 
 import cn.roy.springcloud.gateway.es.bean.ApiCallBean;
+import com.alibaba.fastjson.JSON;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.ResponseListener;
+import org.elasticsearch.client.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
@@ -17,6 +23,9 @@ import java.util.Date;
  */
 public class PostFilter extends ZuulFilter {
     private static Logger log = LoggerFactory.getLogger(PostFilter.class);
+
+    @Autowired
+    private RestClient restClient;
 
     @Override
     public String filterType() {
@@ -40,7 +49,10 @@ public class PostFilter extends ZuulFilter {
         Date endTime = new Date();
         Date startTime = RequestHolder.getStartDate();
         long time = endTime.getTime() - startTime.getTime();
+        log.info("请求结束，请求ID：{}，开始时间：{}，结束时间：{}，请求耗时{}", request.hashCode(), startTime, endTime, time);
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
 
+        // 存储到ES
         ApiCallBean apiCallBean = new ApiCallBean();
         apiCallBean.setName(request.getRequestURI());
         apiCallBean.setStartTime(startTime);
@@ -48,8 +60,20 @@ public class PostFilter extends ZuulFilter {
         apiCallBean.setCostTime(time);
         apiCallBean.setRemoteIp(request.getRemoteAddr());
 
-        log.info("请求结束，请求ID：{}，开始时间：{}，结束时间：{}，请求耗时{}", request.hashCode(), startTime, endTime, time);
-        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        Request esRequest = new Request("POST", "/api/_doc/");
+        esRequest.setJsonEntity(JSON.toJSONString(apiCallBean));
+        restClient.performRequestAsync(esRequest, new ResponseListener() {
+            @Override
+            public void onSuccess(Response response) {
+                log.info("保存成功");
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                log.info("保存失败");
+            }
+        });
+
         return null;
     }
 
