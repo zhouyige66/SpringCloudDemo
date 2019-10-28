4fa96c614edc59e162c12e840ff4d6890c143083
Roy Z Zhou <roy.z.zhou@cn.pwc.com>
2019-10-28 13:46:32 +0800

    mq pom修改！

--- gateway/src/main/java/cn/roy/springcloud/gateway/shiro/ShiroRealm.java
+++ gateway/src/main/java/cn/roy/springcloud/gateway/shiro/ShiroRealm.java
@@ -18,47 +18,46 @@ import java.util.Set;
  */
 public class ShiroRealm extends AuthorizingRealm {
 
-    /**
-     * 方面用于加密 参数：AuthenticationToken是从表单穿过来封装好的对象
-     */
+    // 认证
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-        System.out.println("doGetAuthenticationInfo:" + token);
-
-        // 将AuthenticationToken强转为AuthenticationToken对象
-        UsernamePasswordToken upToken = (UsernamePasswordToken) token;
-        // 获得从表单传过来的用户名
-        String username = upToken.getUsername();
-        // 从数据库查看是否存在用户
-        UserService userService = new UserService();
-        // 如果用户不存在，抛此异常
-        if (!userService.selectUsername(username)) {
-            throw new UnknownAccountException("无此用户名！");
-        }
-
-        // 认证的实体信息，可以是username，也可以是用户的实体类对象，这里用的用户名
-        Object principal = username;
-        // 从数据库中查询的密码
-        Object credentials = userService.selectPassword(username);
-        // 颜值加密的颜，可以用用户名
-        ByteSource credentialsSalt = ByteSource.Util.bytes(username);
-        // 当前realm对象的名称，调用分类的getName()
-        String realmName = this.getName();
-
-        // 创建SimpleAuthenticationInfo对象，并且把username和password等信息封装到里面
-        // 用户密码的比对是Shiro帮我们完成的
-        SimpleAuthenticationInfo info = null;
-        info = new SimpleAuthenticationInfo(principal, credentials, credentialsSalt, realmName);
-        return info;
+//        System.out.println("doGetAuthenticationInfo:" + token);
+//
+//        // 将AuthenticationToken强转为AuthenticationToken对象
+//        UsernamePasswordToken upToken = (UsernamePasswordToken) token;
+//        // 获得从表单传过来的用户名
+//        String username = upToken.getUsername();
+//        if(!username.equals("kk20")){
+//            throw new UnknownAccountException("无此用户名！");
+//        }
+//
+//        // 从数据库查看是否存在用户
+//        UserService userService = new UserService();
+//        // 如果用户不存在，抛此异常
+//        if (!userService.selectUsername(username)) {
+//            throw new UnknownAccountException("无此用户名！");
+//        }
+//
+//        // 认证的实体信息，可以是username，也可以是用户的实体类对象，这里用的用户名
+//        Object principal = username;
+//        // 从数据库中查询的密码
+//        Object credentials = userService.selectPassword(username);
+//        // 颜值加密的颜，可以用用户名
+//        ByteSource credentialsSalt = ByteSource.Util.bytes(username);
+//        // 当前realm对象的名称，调用分类的getName()
+//        String realmName = this.getName();
+//
+//        // 创建SimpleAuthenticationInfo对象，并且把username和password等信息封装到里面
+//        // 用户密码的比对是Shiro帮我们完成的
+//        SimpleAuthenticationInfo info = null;
+//        info = new SimpleAuthenticationInfo(principal, credentials, credentialsSalt, realmName);
+//        return info;
+        return null;
     }
 
-    // 用于授权
+    // 授权
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-        // User user=(User)
-        // principals.fromRealm(this.getClass().getName()).iterator().next();//获取session中的用户
-        // System.out.println("在MyShiroRealm中AuthorizationInfo（授权）方法中从session中获取的user对象:"+user);
-
         // 从PrincipalCollection中获得用户信息
         Object principal = principals.getPrimaryPrincipal();
         System.out.println("ShiroRealm  AuthorizationInfo:" + principal.toString());
