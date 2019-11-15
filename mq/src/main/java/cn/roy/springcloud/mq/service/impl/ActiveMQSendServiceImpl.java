72272528e054ea8bfa985eedef0a3f5979d6a6e4
Roy <751664206@qq.com>
2019-11-15 15:04:38 +0800

    job pom文件添加注释

--- mq/src/main/java/cn/roy/springcloud/mq/service/impl/ActiveMQSendServiceImpl.java
+++ mq/src/main/java/cn/roy/springcloud/mq/service/impl/ActiveMQSendServiceImpl.java
@@ -1,12 +1,16 @@
 package cn.roy.springcloud.mq.service.impl;
 
 import cn.roy.springcloud.mq.service.ActiveMQSendService;
+import org.apache.activemq.ScheduledMessage;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.core.MessagePostProcessor;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.jms.JMSException;
 
 /**
  * @Description:
@@ -23,6 +27,21 @@ public class ActiveMQSendServiceImpl implements ActiveMQSendService {
     @Override
     public void send2Queue(String queue, Object obj) {
         jmsMessagingTemplate.convertAndSend(new ActiveMQQueue(queue), obj);
+
+        jmsMessagingTemplate.getJmsTemplate().convertAndSend(new ActiveMQQueue(""), obj,
+                new org.springframework.jms.core.MessagePostProcessor() {
+                    @Override
+                    public javax.jms.Message postProcessMessage(javax.jms.Message message) throws JMSException {
+                        message.setLongProperty(ScheduledMessage.AMQ_SCHEDULED_DELAY, 10 * 1000);
+                        return message;
+                    }
+                });
+        jmsMessagingTemplate.convertAndSend(new ActiveMQQueue(""), obj, new MessagePostProcessor() {
+            @Override
+            public Message<?> postProcessMessage(Message<?> message) {
+                return message;
+            }
+        });
     }
 
     @Override
