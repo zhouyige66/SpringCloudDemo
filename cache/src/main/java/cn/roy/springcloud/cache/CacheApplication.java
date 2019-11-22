db0b5a98df6ebbfe32c34ac289424637bf9d7e0a
Roy Z Zhou <roy.z.zhou@cn.pwc.com>
2019-11-22 14:30:50 +0800

    cache整理

--- cache/src/main/java/cn/roy/springcloud/cache/CacheApplication.java
+++ cache/src/main/java/cn/roy/springcloud/cache/CacheApplication.java
@@ -6,8 +6,6 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
-import org.springframework.cache.interceptor.KeyGenerator;
-import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -28,11 +26,6 @@ public class CacheApplication {
     @Bean
     public CacheManager cacheManager(){
         return new EhCacheCacheManager(ehCacheManagerFactoryBean().getObject());
-    }
-
-    @Bean
-    public KeyGenerator keyGenerator(){
-        return new SimpleKeyGenerator();
     }
 
 }
--- gateway/src/main/java/cn/roy/springcloud/gateway/config/CacheConfig.java
+++ gateway/src/main/java/cn/roy/springcloud/gateway/config/CacheConfig.java
@@ -4,7 +4,6 @@ import cn.roy.springcloud.gateway.cache.MultipleCacheManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
