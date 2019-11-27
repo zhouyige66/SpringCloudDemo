a25b436103cc83a2cdcaf5328b894c9ee5e7ada4
Roy <751664206@qq.com>
2019-11-27 14:50:47 +0800

    缓存过期处理

--- cache/src/main/java/cn/roy/springcloud/cache/cache/CacheConfig.java
+++ cache/src/main/java/cn/roy/springcloud/cache/cache/CacheConfig.java
@@ -3,16 +3,21 @@ package cn.roy.springcloud.cache.cache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyExpiredEvent;
+import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.session.web.http.CookieSerializer;
@@ -30,6 +35,8 @@ import java.time.Duration;
 public class CacheConfig {
     private static final Logger logger = LoggerFactory.getLogger(CacheConfig.class);
 
+    @Autowired
+    private ApplicationContext applicationContext;
     @Autowired
     private RedisConnectionFactory redisConnectionFactory;
 
@@ -56,8 +63,29 @@ public class CacheConfig {
 //        redisMessageListenerContainer.addMessageListener((Message message, byte[] pattern) -> {
 //            logger.info("收到keyspace消息：" + message);
 //        }, new PatternTopic("__keyspace@*__:*"));
-
         return redisMessageListenerContainer;
+    }
+
+    @Bean
+    public void registerRedisKeyExpiredListener() {
+        RedisMessageListenerContainer container = redisMessageListenerContainer();
+        KeyExpirationEventMessageListener listener = new KeyExpirationEventMessageListener(container);
+        listener.init();
+        listener.setApplicationEventPublisher(applicationContext);
+    }
+
+    @EventListener(classes = RedisKeyExpiredEvent.class)
+    public void listener(RedisKeyExpiredEvent expiredEvent) {
+        logger.info("监听到过期事件：" + expiredEvent);
+        String cacheName = expiredEvent.getKeyspace();
+        String key = new String(expiredEvent.getId());
+
+        EhCacheCacheManager ehCacheCacheManager = ehCacheCacheManager();
+        Cache cache = ehCacheCacheManager.getCache(cacheName);
+        if (null != cache) {
+            logger.info("清除EhCache中缓存");
+            cache.evict(key);
+        }
     }
 
     @Bean
