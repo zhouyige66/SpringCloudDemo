9b50cf6164480f72bf91d0fef57a04ac1a759260
Roy Z Zhou <roy.z.zhou@cn.pwc.com>
2019-12-04 10:51:07 +0800

    配置中心认证密码加密

---
+++ config/src/main/java/cn/roy/springcloud/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package cn.roy.springcloud.config;
+
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Description: 安全配置
+ * @Author: Roy Z
+ * @Date: 2019-12-04 10:36
+ * @Version: v1.0
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        //ignore
+        web.ignoring().antMatchers("/encrypt","/encrypt/*", "/health", "/decrypt");
+    }
+
+}
