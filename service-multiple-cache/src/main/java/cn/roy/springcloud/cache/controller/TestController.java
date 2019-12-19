d5eeec344d3db9a7ca82760dabe22c52fa721413
Roy Z Zhou <roy.z.zhou@cn.pwc.com>
2019-12-19 15:25:06 +0800

    添加缓存过期时间测试

--- service-multiple-cache/src/main/java/cn/roy/springcloud/cache/controller/TestController.java
+++ service-multiple-cache/src/main/java/cn/roy/springcloud/cache/controller/TestController.java
@@ -4,11 +4,15 @@ import cn.roy.springcloud.base.dto.SimpleDto;
 import cn.roy.springcloud.base.http.ResultData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description: 测试
@@ -20,6 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("test")
 public class TestController {
     private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Cacheable(value = "user", key = "'name_'+#id")
     @GetMapping("user/{id}")
@@ -33,6 +40,12 @@ public class TestController {
         SimpleDto dto = new SimpleDto();
         dto.setValue(String.valueOf(id));
         return ResultData.success(dto);
+    }
+
+    @GetMapping("cache/{value}/{time}")
+    public ResultData cache(@PathVariable String value, @PathVariable long time) {
+        redisTemplate.opsForValue().set("StatusCheck:" + value, value, time, TimeUnit.SECONDS);
+        return ResultData.success();
     }
 
 }
