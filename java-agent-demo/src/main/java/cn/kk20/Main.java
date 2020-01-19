19e746083ae8a5f164c147a7b7d7195103fb7113
Roy Z Zhou <roy.z.zhou@cn.pwc.com>
2020-01-19 15:58:25 +0800

    多线程测试

--- java-agent-demo/src/main/java/cn/kk20/Main.java
+++ java-agent-demo/src/main/java/cn/kk20/Main.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @Description:
  * @Author: Roy Z
@@ -16,6 +19,11 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
+
+        // 多线程测试
+        ExecutorService executorService = Executors.newWorkStealingPool();
+
+
     }
 
 }
