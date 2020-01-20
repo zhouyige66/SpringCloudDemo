7454dfd8a087401d2c25c82eb34a37e159a2abbf
Roy Z Zhou <roy.z.zhou@cn.pwc.com>
2020-01-20 17:30:52 +0800

    线程池测试

--- java-agent-demo/src/main/java/cn/kk20/MainApp.java
+++ java-agent-demo/src/main/java/cn/kk20/MainApp.java
@@ -23,13 +23,39 @@ public class MainApp {
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
 
-        // 多线程测试
-        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(5);
-        ExecutorService workStealingPool = Executors.newWorkStealingPool();
+        // 线程池
+        // 几种通用的线程池
+        /**
+         * 创建一个单工作线程执行的线程池，返回的实例不可再重新配置
+         * 构建是包装ThreadPoolExecutor，队列使用LinkedBlockingQueue
+         */
         ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        /**
+         * 创建一个重用固定数量线程的线程池，操作一个共享的无限队列。
+         * 线程存储队列使用LinkedBlockingQueue
+         */
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(5);
+        /**
+         * 创建根据需要创建新线程的线程池，可复用先前创建的线程（如果可用），无可复用线程，则新建线程然后加入线程池
+         * 几个重要参数：0个核心线程，最大线程数Integer.MAX_VALUE，线程空闲60S则被移出线程池
+         * 队列使用SynchronousQueue，实际上它不是一个真正的队列，因为它不会为队列中元素维护存储空间。与其他队列不同的是，它维护
+         * 一组线程，这些线程在等待着把元素加入或移出队列。在使用SynchronousQueue作为工作队列的前提下，客户端代码向线程池提交任
+         * 务时，而线程池中又没有空闲的线程能够从SynchronousQueue队列实例中取一个任务，那么相应的offer方法调用就会失败（即任务
+         * 没有被存入工作队列）。此时，ThreadPoolExecutor会新建一个新的工作者线程用于对这个入队列失败的任务进行处理（假设此时
+         * 线程池的大小还未达到其最大线程池大小maximumPoolSize）
+         */
         ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        /**
+         * 创建并行运行线程池（处理器核心数）
+         */
+        ExecutorService workStealingPool = Executors.newWorkStealingPool();
+        /**
+         * 可定期运行的线程池
+         */
         ScheduledExecutorService singleThreadScheduledExecutor = Executors.newSingleThreadScheduledExecutor();
         ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(1);
+
+
 
         Executors.unconfigurableExecutorService(new ExecutorService() {
             @Override
